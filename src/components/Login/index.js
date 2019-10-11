eda626b214119c38071dcfa369989ffeef485892
Filip <filip.k.wanat@gmail.com>
2019-10-11 21:43:16 +0200

    Add login form

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Login = () => {
-    return <div> LOGIN </div>;
+    return (
+        <div>
+            <form id="formLogin">
+                <label htmlFor="loginInput">Login:</label>
+                <input type="text" id="loginInput" placeholder="Login" required />
+                <label htmlFor="passwordInput">Password:</label>
+                <input type="password" id="passwordInput" placeholder="Password" required />
+                <button type="submit" form="formLogin">
+                    Login
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default Login;
