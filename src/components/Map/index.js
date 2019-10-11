b8a728c512c2e4936b8265882d5cc2636ac885a5
Filip <filip.k.wanat@gmail.com>
2019-10-11 21:43:36 +0200

    Add task list in map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Map = () => {
-    return <div> Map </div>;
+    return (
+        <div>
+            <ul>
+                <li>Dział 1</li>
+                <li>Dział 2</li>
+                <li>Dział 3</li>
+            </ul>
+        </div>
+    );
 };
 
 export default Map;
