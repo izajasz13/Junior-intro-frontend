e117b724d9e85c83cb83f2b67e3370318d99a3a9
Filip <filip.k.wanat@gmail.com>
2019-10-11 22:03:09 +0200

    Add tasks sub-components

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import UserBar from './UserBar';
+import TaskList from './TaskList';
+import Task from './Task';
 
 const Tasks = () => {
-    return <div> Tasks </div>;
+    return (
+        <div>
+            <UserBar />
+            <TaskList />
+            <Task />
+        </div>
+    );
 };
 
 export default Tasks;
