e2a5808774eed3da2c60a146f523e9ff1fe94582
Adam Oleszko <adamoleszko44@gmail.com>
2019-10-12 02:40:52 +0200

    in progress

--- src/components/Tasks/UserBar.js
+++ src/components/Tasks/UserBar.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import './UserBar.css'
+
 class UserBar extends React.Component {
     render() {
-        return <div> UserBar </div>;
+        const {user} = this.props
+
+        return (
+            <div className="user-bar-container">
+                <div>
+                    {/* {user.name} */}
+                    Adam
+                </div>
+                <div>
+                <div class="ui indicating progress active" data-percent="50">
+                    <div class="bar"></div>
+                    <div class="label">Postęp</div>
+                </div>
+                </div>
+                <div>
+                    {/* {user.coins} coins */}
+                    100 <img src="dollar.png" />
+                </div>
+                <div>
+                    1000 <img src="goal.png" />
+                </div>
+                <div>
+                    <button className="ui button">Logout</button>
+                </div>
+            </div>
+        );
     }
 }
 
@@ -11,7 +38,4 @@ const mapStateToProps = state => {
     return { user: state.user, section: state.section };
 };
 
-export default connect(
-    mapStateToProps,
-    {}
-)(UserBar);
+export default connect(mapStateToProps)(UserBar);
