06a7029cec291992f3f01f9ec7891dda1366bce7
Filip <filip.k.wanat@gmail.com>
2019-10-12 07:02:32 +0200

    Destroy error - no key warnign

--- src/components/Tasks/UserBar.js
+++ src/components/Tasks/UserBar.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUser, logout, fetchKnowledgeBaseList } from '../../actions';
-import { Modal } from 'semantic-ui-react'
+import { Modal } from 'semantic-ui-react';
 
 import './UserBar.css';
 
 class UserBar extends React.Component {
     state = {
-        isModalOpen: false,
-    }
+        isModalOpen: false
+    };
 
     componentDidMount() {
         if (!this.props.user.name) this.props.fetchUser(localStorage.getItem('userId'));
 
-        this.props.fetchKnowledgeBaseList()
+        this.props.fetchKnowledgeBaseList();
     }
 
     clickLogout = event => {
@@ -21,67 +21,63 @@ class UserBar extends React.Component {
     };
 
     handleToggleModal = () => {
-        this.setState({isModalOpen: !this.state.isModalOpen})
-    }
+        this.setState({ isModalOpen: !this.state.isModalOpen });
+    };
 
     render() {
         const { user } = this.props;
-        console.log(this.props.knowledgeList)
+        console.log(this.props.knowledgeList);
 
         return (
             <>
-            <div className="user-bar-container">
-                <div>{user.name}</div>
-                <div>
-                    <div className="ui indicating progress active" data-percent="50">
-                        <div className="bar"></div>
-                        <div className="label">Postęp</div>
-                    </div>
-                </div>
-                <div>
-                    {user.coins}
-                    <img src="dollar.png" alt="dollar" />
-                </div>
-                <div>
-                    {user.experience} <img src="goal.png" alt="goal" />
-                </div>
-                <div className="right-buttons">
-                    <div className="spellbook" onClick={this.handleToggleModal}>
-                        <img src="open-book.png" alt="open book" />
-                        <div>Księga zaklęć</div>
+                <div className="user-bar-container">
+                    <div>{user.name}</div>
+                    <div>
+                        <div className="ui indicating progress active" data-percent="50">
+                            <div className="bar"></div>
+                            <div className="label">Postęp</div>
+                        </div>
                     </div>
                     <div>
-                        <button className="ui button" onClick={this.clickLogout}>
-                            <img src="logout.png" alt="logout" />
-                        </button>
+                        {user.coins}
+                        <img src="dollar.png" alt="dollar" />
+                    </div>
+                    <div>
+                        {user.experience} <img src="goal.png" alt="goal" />
+                    </div>
+                    <div className="right-buttons">
+                        <div className="spellbook" onClick={this.handleToggleModal}>
+                            <img src="open-book.png" alt="open book" />
+                            <div>Księga zaklęć</div>
+                        </div>
+                        <div>
+                            <button className="ui button" onClick={this.clickLogout}>
+                                <img src="logout.png" alt="logout" />
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <Modal open={this.state.isModalOpen} style={{background: 'black'}}>
-            <Modal.Header>
-                <div onClick={this.handleToggleModal} className="modal-header">
-                    <span>
-                        X
-                    </span>
-                </div>
-            </Modal.Header>
-                <Modal.Content>
-                <div className="content-inner">
-                    <div className="left-list">
-                        {this.props.knowledgeList.map(el => {
-                            return (
-                                <div className="left-list-element">
-                                    {el.title}
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div className="right-content">
-                        asdasdasdasdasd
-                    </div>
-                </div>
-                </Modal.Content>
-            </Modal>
+                <Modal open={this.state.isModalOpen} style={{ background: 'black' }}>
+                    <Modal.Header>
+                        <div onClick={this.handleToggleModal} className="modal-header">
+                            <span>X</span>
+                        </div>
+                    </Modal.Header>
+                    <Modal.Content>
+                        <div className="content-inner">
+                            <div className="left-list">
+                                {this.props.knowledgeList.map(el => {
+                                    return (
+                                        <div className="left-list-element" key={el.knowledgeId}>
+                                            {el.title}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            <div className="right-content">asdasdasdasdasd</div>
+                        </div>
+                    </Modal.Content>
+                </Modal>
             </>
         );
     }
